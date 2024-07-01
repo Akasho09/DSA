60f4973bd1bdf3e73228d55f5d023f3b052a7d4e
Akasho09 <aakashbwd@gmail.com>
2024-07-01 19:26:02 +0530

    hi

--- maxSubarray.cpp
+++ maxSubarray.cpp
@@ -78,3 +78,26 @@ public:
     }
 };
 */
+
+
+/*
+
+class Solution {
+public:
+int maxSubArray(vector<int>& nums) {
+int sum=0;
+int ans=INT_MIN;
+for (int i=0;i<nums.size();i++){
+sum+=nums[i];
+if (sum>ans){
+    ans=sum;
+}
+    if (sum<0){
+        sum=0;
+}
+}
+return ans; 
+}
+};
+
+*/
