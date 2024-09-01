0d4750608cbd617671520869556f0a132858255d
Akasho09 <aakashbwd@gmail.com>
2024-09-01 22:42:32 +0530

    hi

--- 2sum.cpp
+++ 2sum.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<int> twoSum(vector<int>& nums, int target) {
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target)  {
 int i=0;
         while (i<nums.size()){
 int j=i+1;
@@ -15,7 +17,17 @@ j++;
 }
  return {};
 }
-
+};
 int main () {
-    
+    vector<int> v;
+v.push_back(1);
+v.push_back(5);
+v.push_back(7);
+v.push_back(6);
+v.push_back(2);
+v.push_back(3);
+v.push_back(6);
+v.push_back(4);
+    Solution a; 
+a.twoSum(v,4);
 }
---
+++ maximumdistance.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+    int maxDistance(vector<int>& nums1, vector<int>& nums2) {
+        int ans=0;
+        for (int i=0;i<nums1.size();i++){
+          for (int j=i+ans;j<nums2.size();j++){
+         if (nums1[i]<=nums2[j]) {
+            if ((j-i)>ans) ans=j-i;
+            }
+          else break;
+        }
+    }
+    return ans;
+    }
+
+int main () {
+vector<int> v;
+v.push_back(55);
+v.push_back(30);
+v.push_back(5);
+v.push_back(4);
+v.push_back(2);
+
+vector<int> v2;
+v2.push_back(100);
+v2.push_back(20);
+v2.push_back(10);
+v2.push_back(10);
+v2.push_back(5);
+
+cout << maxDistance(v,v2);
+
+}
