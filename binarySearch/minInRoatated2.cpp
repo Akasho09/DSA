1a264dc69f729a5907851fca37c501ae3d9e26d4
Akasho09 <aakashbwd@gmail.com>
2025-01-07 06:03:23 +0530

    h

--- binarySearch/minInRoatated2.cpp
+++ binarySearch/minInRoatated2.cpp
@@ -5,18 +5,25 @@ using namespace std ;
     if(n==1) return nums[0];
     int s=0;
     int e=n-1;
-    int m;
+    int a = INT_MAX;
+    while(nums[s]==nums[e] && s<=e) {
+    a=min(a,nums[s]);
+    n=e;
+    e--; s++;
+    }
+    int m=s+(e-s)/2;
     while(s<=e){
     m=s+(e-s)/2;
     if(nums[m]>nums[n-1]) s=m+1;
     else e=m-1;
     }
-    // int left = (m > 0) ? nums[m - 1] : INT_MAX;
+    n=nums.size();
+    int left = (m > 0) ? nums[m - 1] : INT_MAX;
     int right = (m < n - 1) ? nums[m + 1] : INT_MAX;
-    return min(nums[m], right);
+    return min({nums[m],right,left,a});
     }
 
 int main () {
-    vector<int>v= {2,2,2,0,1,2,2,2};
+    vector<int>v= {2,2,2,0,0,0,1,1,2,2};
     cout << findMin(v);
 }
